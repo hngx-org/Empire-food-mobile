2033a02d97e3286cec4229f6d3fd1ce9c4b69b0c
victormystery <osarobovictors@outlook.com>
2023-09-20 10:54:48 +0100

    Added Gift Free Lunch Screens

--- lib/screens/gift_lunch/gift_free_lunch_2.dart
+++ lib/screens/gift_lunch/gift_free_lunch_2.dart
@@ -15,70 +15,72 @@ class _GiftFreeLunchScreen2State extends State<GiftFreeLunchScreen2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.only(top: 70, right: 20, left: 20),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.arrow_back_ios),
-              ),
-              SizedBox(width: 60),
-              Text(
-                'Gift Free Lunch',
-                style: TextStyle(
-                  color: Color(0xFF583208),
-                  fontSize: 24,
-                  fontFamily: 'Tropiline',
-                  fontWeight: FontWeight.w700,
-                  height: 1.0,
-                  letterSpacing: 0.24,
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 70, right: 20, left: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back_ios),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          SizedBox(
-            width: 380,
-            child: Text(
-              'Please ensure that you provide accurate information in this form to avoid any hiccups in this process.',
-              style: GoogleFonts.nunito(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-              textAlign: TextAlign.justify,
+                SizedBox(width: 60),
+                Text(
+                  'Gift Free Lunch',
+                  style: TextStyle(
+                    color: Color(0xFF583208),
+                    fontSize: 24,
+                    fontFamily: 'Tropiline',
+                    fontWeight: FontWeight.w700,
+                    height: 1.0,
+                    letterSpacing: 0.24,
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 20),
-          RecipientInfoSection(),
-          SizedBox(height: 30),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              buttonPop(
-                98,
-                51,
-                Color(0xFFF5FAFF),
-                'Cancel',
-                context,
+            SizedBox(height: 20),
+            SizedBox(
+              width: 380,
+              child: Text(
+                'Please ensure that you provide accurate information in this form to avoid any hiccups in this process.',
+                style: GoogleFonts.nunito(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.justify,
               ),
-              buttonNext(
-                84,
-                51,
-                Color(0xFFEAD9FC),
-                'Next',
-                context,
-                GiftFreeLunchScreen3(),
-              ),
-            ],
-          )
-        ],
+            ),
+            SizedBox(height: 20),
+            RecipientInfoSection(),
+            SizedBox(height: 30),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buttonPop(
+                  98,
+                  51,
+                  Color(0xFFF5FAFF),
+                  'Cancel',
+                  context,
+                ),
+                buttonNext(
+                  84,
+                  51,
+                  Color(0xFFEAD9FC),
+                  'Next',
+                  context,
+                  GiftFreeLunchScreen3(),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
